24614c2e44105c20dca141159c5f1e2f3505e9c1
David Bethune and Jon Toler <jon@>
2016-08-17 13:28:26 -0700

    adding in functions, and methods for if, do while loops using JS

---
+++ README.md
@@ -0,0 +1,54 @@
+README.md
+
+# _PigFace_
+
+#### _Used branching logic to display different elements on a webpage 08.17.2016_
+
+#### By _**David Bethune and Jon Toler**_
+
+## Description
+Converting to pig latin:
+
+1. For words beginning with a vowel, add "ay" to the end.
+_EXAMPLE_
+input: and
+ouput: anday
+
+2. For words beginning with one or more consonants, move all of the first consecutive consonants to the end, and add "ay".
+_EXAMPLE_
+input: tree
+output: eetray
+
+3. If the first consonants include "qu", move the "u" along with the "q". Don't forget about words like "squeal" where "qu" doesn't come first!
+_EXAMPLE_
+input: quality
+output: alityquay
+
+4. For words beginning with "y", treat "y" as a consonant.
+_EXAMPLE_
+input: yes
+output: esyay
+
+5. The program does nothing to non-alphabetical characters, since they do not contain consonants or vowels.
+_EXAMPLE_
+input: 1
+output: 1
+
+
+## Known Bugs
+
+_N/A_
+
+## Technologies Used
+
+_Atom_
+_HTML_
+_CSS_
+_Javascript_
+_jQuery_
+
+### License
+
+_This software is licensed under the MIT license._
+
+Copyright (c) 2016 Stephen Newkirk and Jon Toler All Rights Reserved.
---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  function translator(input){
+    var vowels = ["a","e","i","o","u"];
+    var specChars = ["!","?",".",","];
+    var i = 0;
+    var consonants = "";
+    var punctuation = "";
+    var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+
+    if(specChars.includes(input[input.length-1])){
+      punctuation = input[input.length-1];
+      input = input.slice(0, input.length-1);
+    }
+    if(!vowels.includes(input[i]) && !numbers.includes(input[i])){
+      do{
+        consonants += input[i];
+        ++i;
+      }while(!(vowels.includes(input[i])) && (input[i] != "y"));
+
+      if((input[i-1]==="q") && (input[i]==="u")){
+        consonants += input[i];
+        ++i;
+      }
+    }else if(!numbers.includes(input[i])){
+      return input + "ay"+punctuation;
+    }else{
+      return input + punctuation;
+    }
+    return input.slice(i)+consonants+"ay"+punctuation;
+  }
+
+  $("#pigFace").submit(function(event){
+    var sentence = $("#pigIn").val().toLowerCase().split(" ");
+
+    var result = [];
+
+    sentence.forEach(function(word) {
+      result += translator(word) + " ";
+    });
+
+    $("#pigOut").text(result);
+    event.preventDefault();
+  });
+});
